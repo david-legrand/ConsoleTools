ce8771c5b71c59216ce5b4141b609b500d3c5bdc
David Legrand <david@nextinpact.com>
2017-09-30 22:48:34 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleTools/Security.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleTools
+{
+    public class Security
+    {
+        public static string GetPassword()
+        {
+            StringBuilder pwd = new StringBuilder();
+
+            while (true)
+            {
+                ConsoleKeyInfo i = Console.ReadKey(true);
+                if (i.Key == ConsoleKey.Enter) break;
+                else if (i.Key == ConsoleKey.Backspace)
+                {
+                    if (pwd.Length > 0)
+                    {
+                        pwd.Remove(pwd.Length - 1, 1);
+                        Console.Write("\b \b");
+                    }
+                }
+                else
+                {
+                    if (char.IsLetterOrDigit(i.KeyChar) || char.IsPunctuation(i.KeyChar) || char.IsSymbol(i.KeyChar) || char.IsWhiteSpace(i.KeyChar))
+                    {
+                        pwd.Append(i.KeyChar);
+                        Console.Write("*");
+                    }
+
+                }
+            }
+
+            return pwd.ToString();
+        }
+    }
+}
---
+++ ConsoleTools/Tools.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleTools
+{
+    public class Tools
+    {
+        public static void CleanLastLine()
+        {
+            Console.Write("\r" + new String(' ', Console.BufferWidth - 1));
+        }
+
+        [ConditionalAttribute("DEBUG")]
+        public static void WaitForTheEnd()
+        {
+            Console.WriteLine("\rAppuyez sur une touche pour continuer...");
+            Console.Read();
+        }
+    }
+}
